a7b00013f4c4f4e37897124e2d5b96d2cd71613b
vemperala <vemperalaonpur@gmail.com>
2017-10-29 22:46:47 -0400

    created a sample project for active Mq with In memory

---
+++ src/main/java/com/akhildevz/ActiveMQ/ActiveMqInMemoryApplication.java
@@ -0,0 +1,12 @@
+package com.akhildevz.ActiveMQ;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActiveMqInMemoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActiveMqInMemoryApplication.class, args);
+	}
+}
